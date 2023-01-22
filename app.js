0a283db4ed4e561dd231844ce571495d84edb4da
shubham sarode <shubham.sarode@extwebtech.in>
2023-01-22 12:19:36 +0530

    created express server on PORT=2111

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-export function add(a, b) {
-  return a + b;
-}
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 2000;
+
+app.get("/", (req, res) => {
+  res.send("Hi i am port " + PORT);
+});
+
+app.listen(PORT, () => {
+  console.log("____listening on port ", PORT);
+});
