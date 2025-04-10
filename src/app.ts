1397bf95018319d3345de06c5f5c745d16089c5d
Shubham Sarode <shubhamsarode435@gmail.com>
2025-04-10 12:24:21 +0530

    Refactor app.ts to serve static files and update root route response

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 const PORT = process.env.PORT || 2000;
 
+app.use(express.static(path.join(__dirname, './Public')))
+
 app.get("/", (req, res) => {
-  res.send("Hello What r u doing " + PORT);
+  res.sendFile("./index.html")
 });
 
 app.listen(PORT, () => {
-  console.log("____listening on port ", PORT);
+  console.log(`____listening on port http://localhost:${PORT}`);
 });
